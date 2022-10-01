4976f0c3df754f9001cce14b9d1049b33598a757
Tamara-spb <111680432+Tamara-spb@users.noreply.github.com>
2022-10-01 20:55:52 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
-﻿// Напишите программу, которая на вход принимает 
-// два числа и выдаёт, какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+﻿// Напишите программу, которая принимает на вход
+//  три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
 
 Console.WriteLine ("Введите первое число");
 int numberA = Convert.ToInt32(Console.ReadLine());
@@ -10,11 +10,18 @@ int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ("Введите второе число");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
-if (numberA > numberB)
+Console.WriteLine ("Введите третье число");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB && numberA > numberC)
 {
-    Console.WriteLine ("Максимальное число" +" " + numberA);
+    Console.WriteLine ("Максимальное число" + " " + numberA);
 }
-else
+else if (numberB > numberA && numberB > numberC)
 {
     Console.WriteLine ("Максимальное число" + " " + numberB);
+}
+else if (numberC > numberA && numberC > numberB)
+{
+    Console.WriteLine ("Максимальное число" + " " + numberC);
 }
