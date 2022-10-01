8470bfa767ff83e2e5285d6bd59d11208d381623
Tamara-spb <111680432+Tamara-spb@users.noreply.github.com>
2022-10-01 20:44:09 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 ﻿// Напишите программу, которая на вход принимает 
-// число (N), а на выходе показывает все чётные числа от 1 до N.
-// 5 -> 2, 4
-// 8 -> 2, 4, 6, 8
+// число и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
 
 Console.WriteLine ("Введите целое число");
-int number = Convert.ToInt32(Console.ReadLine());
+int numberA = Convert.ToInt32(Console.ReadLine());
 
-int count = 1;
-while (count < number+1)
+if (numberA % 2 == 0)
 {
-    Console.Write (count + " ");
-    count +=1;
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine ("Нет");
 }
